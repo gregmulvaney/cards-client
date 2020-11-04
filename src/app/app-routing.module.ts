83ea3a9496ecef6375b42846cebe75b64dbb2791
Gregory Mulvaney <gregtmulvaney@gmail.com>
2020-11-04 00:09:54 -0600

    pudding

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { IndexComponent } from './index/index.component';
 import { CreateComponent } from './create/create.component';
+import { JoinComponent } from './join/join.component';
 
 const routes: Routes = [
   { path: '', component: IndexComponent },
   { path: 'game/create', component: CreateComponent },
+  { path: 'game/join', component: JoinComponent },
 ];
 
 @NgModule({
