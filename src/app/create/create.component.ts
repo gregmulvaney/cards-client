be1705a6b7ae4489ca0fa543455cdd345d765c7d
Gregory Mulvaney <gregtmulvaney@gmail.com>
2020-11-05 21:12:13 -0600

    Updated CSS

--- src/app/create/create.component.ts
+++ src/app/create/create.component.ts
@@ -3,13 +3,20 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-create',
   templateUrl: './create.component.html',
-  styleUrls: ['./create.component.scss']
+  styleUrls: ['./create.component.scss'],
 })
 export class CreateComponent implements OnInit {
+  protected name: string;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  handleNameChange(event): void {
+    this.name = event.target.value;
   }
 
+  handleSubmit(event): void {
+    event.preventDefault();
+    console.log(`Name: ${this.name}`);
+  }
+
+  ngOnInit(): void {}
 }
